a801d55ca2fcb24d06fabd2adec8aab8c893d528
David JULIEN <david.julien@gmail.com>
2020-06-15 15:58:24 +0200

    - Add error cases for ING bank module (client id/birthdate mismatched and invalid password). - Fix httpc call to fetch keypad image - Add a connection test without mocking ing_keypad tools - Add a real connection test (credentials must be provided in a file named test/banks_fetch_bank_ing_SUITE_data/ing_real_credential.hrl)

--- src/banks_fetch_bank_ing.erl
+++ src/banks_fetch_bank_ing.erl
@@ -26,21 +26,38 @@ connect(ClientId, {ClientPassword, ClientBirthDate}) ->
   case lists:keyfind("ingdf-auth-token", 1, Headers0) of
     {_, AuthToken} -> {ok, {bank_auth, ?MODULE, AuthToken}};
     false ->
-      {ok, {{_Version1, 200, _ReasonPhrase1}, _Headers1, _Body1}} = httpc:request(post, {"https://m.ing.fr/secure/api-v1/login/cif", ?HEADERS, "application/json;charset=UTF-8", "{\"cif\":\""++ClientId++"\",\"birthDate\":\""++ClientBirthDate++"\"}"}, [{timeout,60000}], []),
-      {ok, {{_Version2, 200, _ReasonPhrase2}, _Headers2, Body2}} = httpc:request(post, {"https://m.ing.fr/secure/api-v1/login/keypad", ?HEADERS, "application/json;charset=UTF-8", "{\"keyPadSize\":{\"width\":2840,\"height\":1136}}"}, [{timeout,60000}], []),
+      case httpc:request(post, {"https://m.ing.fr/secure/api-v1/login/cif", ?HEADERS, "application/json;charset=UTF-8", "{\"cif\":\""++ClientId++"\",\"birthDate\":\""++ClientBirthDate++"\"}"}, [{timeout,60000}], []) of
+        {ok,{{"HTTP/1.1",412,"Precondition Failed"}, _Headers1, BodyError}} ->
+          decode_body_error(BodyError);
+        {ok, {{_Version1, 200, _ReasonPhrase1}, _Headers1, _Body1}} ->
+          {ok, {{_Version2, 200, _ReasonPhrase2}, _Headers2, Body2}} = httpc:request(post, {"https://m.ing.fr/secure/api-v1/login/keypad", ?HEADERS, "application/json;charset=UTF-8", "{\"keyPadSize\":{\"width\":2840,\"height\":1136}}"}, [{timeout,60000}], []),
 
-      JSON = jsx:decode(list_to_binary(Body2)),
-      {_, KeypadURL} = lists:keyfind(<<"keyPadUrl">>, 1, JSON),
-      {_, PinPositions} = lists:keyfind(<<"pinPositions">>, 1, JSON),
+          JSON = jsx:decode(list_to_binary(Body2)),
+          {_, KeypadURL} = lists:keyfind(<<"keyPadUrl">>, 1, JSON),
+          {_, PinPositions} = lists:keyfind(<<"pinPositions">>, 1, JSON),
 
-      FullKeypadURL = "https://m.ing.fr/secure/api-v1"++binary_to_list(KeypadURL),
-      {ok, {{_Version3, 200, _ReasonPhrase3}, _Headers3, Body3}} = httpc:request(get, {FullKeypadURL, ?HEADERS, "image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5", ""}, [{timeout,60000}], []),
+          FullKeypadURL = "https://m.ing.fr/secure/api-v1"++binary_to_list(KeypadURL),
+          {ok, {{_Version3, 200, _ReasonPhrase3}, _Headers3, Body3}} = httpc:request(get, {FullKeypadURL, lists:keyreplace("Accept", 1, ?HEADERS, {"Accept", "image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5"})}, [{timeout,60000}], []),
 
-      ClickPositions = banks_fetch_bank_ing_keypad:resolve_keypad(list_to_binary(Body3), PinPositions, ClientPassword),
-      ClickPositionsStr = binary_to_list(jsx:encode([{<<"clickPositions">>,ClickPositions}])),
+          ClickPositions = banks_fetch_bank_ing_keypad:resolve_keypad(list_to_binary(Body3), PinPositions, ClientPassword),
+          ClickPositionsStr = binary_to_list(jsx:encode([{<<"clickPositions">>,ClickPositions}])),
 
-      {ok, {{_Version4, 200, _ReasonPhrase4}, Headers4, _Body4}} = httpc:request(post, {"https://m.ing.fr/secure/api-v1/login/sca/pin", ?HEADERS, "application/json;charset=UTF-8", ClickPositionsStr}, [], []),
-      {_, AuthToken} = lists:keyfind("ingdf-auth-token", 1, Headers4),
+          case httpc:request(post, {"https://m.ing.fr/secure/api-v1/login/sca/pin", ?HEADERS, "application/json;charset=UTF-8", ClickPositionsStr}, [], []) of
+            {ok,{{"HTTP/1.1",412,"Precondition Failed"}, _Headers4, BodyError}} ->
+              decode_body_error(BodyError);
+            {ok, {{_Version4, 200, _ReasonPhrase4}, Headers4, _Body4}} ->
+              {_, AuthToken} = lists:keyfind("ingdf-auth-token", 1, Headers4),
+              {ok, {bank_auth, ?MODULE, AuthToken}}
+          end
+      end
+  end.
 
-      {ok, {bank_auth, ?MODULE, AuthToken}}
+
+decode_body_error(BodyError) ->
+  JSON = jsx:decode(list_to_binary(BodyError)),
+  {_, Error} = lists:keyfind(<<"error">>, 1, JSON),
+  {_, Code} = lists:keyfind(<<"code">>, 1, Error),
+  case Code of
+    <<"AUTHENTICATION.INVALID_CIF_AND_BIRTHDATE_COMBINATION">> -> {error, invalid_birthdate};
+    <<"SCA.WRONG_AUTHENTICATION">> -> {error, invalid_password}
   end.
--- test/banks_fetch_bank_ing_SUITE.erl
+++ test/banks_fetch_bank_ing_SUITE.erl
@@ -1,39 +1,86 @@
 -module(banks_fetch_bank_ing_SUITE).
 -include_lib("common_test/include/ct.hrl").
 -export([
-         all/0, 
+         all/0,
          init_per_testcase/2,
          end_per_testcase/2,
          should_not_authenticate_again_if_token_is_available/1,
+         should_not_authenticate_if_birthdate_and_client_id_mismatched/1,
+         should_not_authenticate_if_password_is_invalid/1,
+         should_connect_without_net_keypad/1,
+         should_connect_without_net/1,
          should_connect/1
         ]).
 
 all() ->
   [
    should_not_authenticate_again_if_token_is_available,
+   should_not_authenticate_if_birthdate_and_client_id_mismatched,
+   should_not_authenticate_if_password_is_invalid,
+   should_connect_without_net_keypad,
+   should_connect_without_net,
    should_connect
   ].
 
-init_per_testcase(_, Config) ->
+init_per_testcase(should_connect_without_net_keypad, Config) ->
+  meck:new(httpc),
+  meck:new(banks_fetch_bank_ing_keypad),
+  Config;
+init_per_testcase(should_connect_without_net, Config) ->
+  meck:new(httpc),
+  Config;
+init_per_testcase(should_connect, Config) ->
+  Config;
+
+init_per_testcase(should_not_authenticate_again_if_token_is_available, Config) ->
+  meck:new(httpc),
+  meck:new(banks_fetch_bank_ing_keypad),
+  Config;
+init_per_testcase(should_not_authenticate_if_birthdate_and_client_id_mismatched, Config) ->
+  meck:new(httpc),
+  meck:new(banks_fetch_bank_ing_keypad),
+  Config;
+init_per_testcase(should_not_authenticate_if_password_is_invalid, Config) ->
   meck:new(httpc),
   meck:new(banks_fetch_bank_ing_keypad),
   Config.
 
-end_per_testcase(_, _Config) ->
+end_per_testcase(should_connect_without_net_keypad, _Config) ->
+  meck:unload(banks_fetch_bank_ing_keypad),
+  meck:unload(httpc),
+  ok;
+end_per_testcase(should_connect_without_net, _Config) ->
+  meck:unload(httpc),
+  ok;
+end_per_testcase(should_connect, _Config) ->
+  ok;
+end_per_testcase(should_not_authenticate_again_if_token_is_available, _Config) ->
+  meck:unload(banks_fetch_bank_ing_keypad),
+  meck:unload(httpc),
+  ok;
+end_per_testcase(should_not_authenticate_if_birthdate_and_client_id_mismatched, _Config) ->
+  meck:unload(banks_fetch_bank_ing_keypad),
+  meck:unload(httpc),
+  ok;
+end_per_testcase(should_not_authenticate_if_password_is_invalid, _Config) ->
   meck:unload(banks_fetch_bank_ing_keypad),
   meck:unload(httpc),
   ok.
 
 -define(CLIENT_ID, "123456789").
--define(CLIENT_PWD, "212121").
+-define(CLIENT_PWD, "234567").
 -define(CLIENT_BIRTHDATE, "230378").
+
+%%
+%% Should not authenticate cases
+%%
 
 should_not_authenticate_again_if_token_is_available(_Config) ->
   ct:comment("Connect to ing account"),
   meck:expect(httpc, request,
-              [ 
-               { 
-                [get, {"https://m.ing.fr/", '_'}, '_', []], 
+              [
+               {
+                [get, {"https://m.ing.fr/", '_'}, '_', []],
                 {ok, {{'fakeversion', 200, 'fakereason'}, [{"ingdf-auth-token", "AUTH_TOKEN"}], 'fakebody'}}
                }
               ]),
@@ -44,8 +91,87 @@ should_not_authenticate_again_if_token_is_available(_Config) ->
 
   ok.
 
+should_not_authenticate_if_birthdate_and_client_id_mismatched(_Config) ->
+  ct:comment("Connect to ing account"),
 
-should_connect(_Config) ->
+  HttpcExpectations = [
+                       % Main page
+                       { [get, {"https://m.ing.fr/", '_'}, '_', []],
+                         {ok, {{'fakeversion', 200, 'fakereason'}, [], 'fakebody'}}
+                       },
+                       % Login
+                       {
+                        [post, {"https://m.ing.fr/secure/api-v1/login/cif", '_', "application/json;charset=UTF-8", "{\"cif\":\""++?CLIENT_ID++"\",\"birthDate\":\""++?CLIENT_BIRTHDATE++"\"}"}, '_', []],
+                        {ok,{{"HTTP/1.1",412,"Precondition Failed"},
+                             fake_headers,
+                             "{\"error\":{\"code\":\"AUTHENTICATION.INVALID_CIF_AND_BIRTHDATE_COMBINATION\",\"message\":\"Votre numéro de client et votre date de naissance ne correspondent pas. Veuillez réessayer.\",\"values\":{}}}"}}
+                       }
+                      ],
+  NbrHttpcExpectations = length(HttpcExpectations),
+  meck:expect(httpc, request, HttpcExpectations),
+  {error, invalid_birthdate} = banks_fetch_bank_ing:connect(?CLIENT_ID, {?CLIENT_PWD, ?CLIENT_BIRTHDATE}),
+  true = meck:validate(httpc),
+  true = meck:validate(banks_fetch_bank_ing_keypad),
+  NbrHttpcExpectations = meck:num_calls(httpc, request, '_'),
+
+  ok.
+
+should_not_authenticate_if_password_is_invalid(_Config) ->
+  KeypadImage = "fake_keypadimage",
+  PinPositions = [1,2,3,4,5,6],
+  PinPositionsStr = "[1,2,3,4,5,6]",
+  ClickPositions = [[1,2],[3,4]],
+  ClickPositionsStr = "[[1,2],[3,4]]",
+
+  ct:comment("Connect to ing account"),
+
+  meck:expect(banks_fetch_bank_ing_keypad, resolve_keypad, fun(MockKeypadImage, MockPinPositions, MockClientPassword) ->
+                                                               KeypadImage = binary_to_list(MockKeypadImage),
+                                                               PinPositions = MockPinPositions,
+                                                               ?CLIENT_PWD = MockClientPassword,
+                                                               ClickPositions
+                                                           end),
+  HttpcExpectations = [
+                       % Main page
+                       { [get, {"https://m.ing.fr/", '_'}, '_', []],
+                         {ok, {{'fakeversion', 200, 'fakereason'}, [], 'fakebody'}}
+                       },
+                       % Login
+                       {
+                        [post, {"https://m.ing.fr/secure/api-v1/login/cif", '_', "application/json;charset=UTF-8", "{\"cif\":\""++?CLIENT_ID++"\",\"birthDate\":\""++?CLIENT_BIRTHDATE++"\"}"}, '_', []],
+                        {ok, {{'fakeversion', 200, 'fakereason'}, 'fakeheaders', 'fakebody'}}
+                       },
+                       % Get keypad info
+                       {
+                        [post, {"https://m.ing.fr/secure/api-v1/login/keypad", '_', "application/json;charset=UTF-8", "{\"keyPadSize\":{\"width\":2840,\"height\":1136}}"}, '_', []],
+                        {ok, {{'fakeversion', 200, 'fakereason'}, 'fakeheaders', "{\"keyPadUrl\":\"/keypad.png\",\"pinPositions\":"++PinPositionsStr++"}"}}
+                       },
+                       % Get keypad image
+                       {
+                        [get, {"https://m.ing.fr/secure/api-v1/keypad.png", '_'}, '_', []],
+                        {ok, {{'fakeversion', 200, 'fakereason'}, 'fakeheaders', KeypadImage}}
+                       },
+                       % Send keypad resolution -> raise wrong authentification
+                       {
+                        [post, {"https://m.ing.fr/secure/api-v1/login/sca/pin", '_', "application/json;charset=UTF-8", "{\"clickPositions\":"++ClickPositionsStr++"}"}, [], []],
+                        {ok,{{"HTTP/1.1",412,"Precondition Failed"}, 'fakeheaders',
+                             "{\"error\":{\"code\":\"SCA.WRONG_AUTHENTICATION\",\"message\":\"Votre authentification est incorrecte. Il vous reste 2 tentatives. Veuillez rÃ©essayer.\",\"values\":{\"numberAttemptsRemaining\":\"2 tentatives\"}}}"}}
+                       }
+                      ],
+  NbrHttpcExpectations = length(HttpcExpectations),
+  meck:expect(httpc, request, HttpcExpectations),
+  {error, invalid_password} = banks_fetch_bank_ing:connect(?CLIENT_ID, {?CLIENT_PWD, ?CLIENT_BIRTHDATE}),
+  true = meck:validate(httpc),
+  true = meck:validate(banks_fetch_bank_ing_keypad),
+  NbrHttpcExpectations = meck:num_calls(httpc, request, '_'),
+
+  ok.
+
+%%
+%% Should connect cases
+%%
+
+should_connect_without_net_keypad(_Config) ->
   KeypadImage = "fake_keypadimage",
   PinPositions = [1,2,3,4,5,6],
   PinPositionsStr = "[1,2,3,4,5,6]",
@@ -61,9 +187,9 @@ should_connect(_Config) ->
                                                                ?CLIENT_PWD = MockClientPassword,
                                                                ClickPositions
                                                            end),
-  HttpcExpectations = [ 
+  HttpcExpectations = [
                        % Main page
-                       { [get, {"https://m.ing.fr/", '_'}, '_', []], 
+                       { [get, {"https://m.ing.fr/", '_'}, '_', []],
                          {ok, {{'fakeversion', 200, 'fakereason'}, [], 'fakebody'}}
                        },
                        % Login
@@ -78,7 +204,7 @@ should_connect(_Config) ->
                        },
                        % Get keypad image
                        {
-                        [get, {"https://m.ing.fr/secure/api-v1/keypad.png", '_', "image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5", ""}, '_', []],
+                        [get, {"https://m.ing.fr/secure/api-v1/keypad.png", '_'}, '_', []],
                         {ok, {{'fakeversion', 200, 'fakereason'}, 'fakeheaders', KeypadImage}}
                        },
                        % Send keypad resolution
@@ -95,3 +221,60 @@ should_connect(_Config) ->
   NbrHttpcExpectations = meck:num_calls(httpc, request, '_'),
 
   ok.
+
+should_connect_without_net(Config) ->
+  ct:comment("Load keypad image"),
+  {ok, KeypadImage} = file:read_file(filename:join([?config(data_dir, Config), "keypad_ing_1.png"])),
+
+  PinPositionsStr = "[1,2,3]",
+  ExpectedClickPositionsStr = "[[1209.5,237.0],[2177.5,712.0],[1693.5,237.0]]",
+  AuthToken = "fake_authtoken",
+
+  ct:comment("Connect to ing account"),
+
+  HttpcExpectations = [
+                       % Main page
+                       { [get, {"https://m.ing.fr/", '_'}, '_', []],
+                         {ok, {{'fakeversion', 200, 'fakereason'}, [], 'fakebody'}}
+                       },
+                       % Login
+                       {
+                        [post, {"https://m.ing.fr/secure/api-v1/login/cif", '_', "application/json;charset=UTF-8", "{\"cif\":\""++?CLIENT_ID++"\",\"birthDate\":\""++?CLIENT_BIRTHDATE++"\"}"}, '_', []],
+                        {ok, {{'fakeversion', 200, 'fakereason'}, 'fakeheaders', 'fakebody'}}
+                       },
+                       % Get keypad info
+                       {
+                        [post, {"https://m.ing.fr/secure/api-v1/login/keypad", '_', "application/json;charset=UTF-8", "{\"keyPadSize\":{\"width\":2840,\"height\":1136}}"}, '_', []],
+                        {ok, {{'fakeversion', 200, 'fakereason'}, 'fakeheaders', "{\"keyPadUrl\":\"/keypad.png\",\"pinPositions\":"++PinPositionsStr++"}"}}
+                       },
+                       % Get keypad image
+                       {
+                        [get, {"https://m.ing.fr/secure/api-v1/keypad.png", '_'}, '_', []],
+                        {ok, {{'fakeversion', 200, 'fakereason'}, 'fakeheaders', binary_to_list(KeypadImage)}}
+                       },
+                       % Send keypad resolution
+                       {
+                        [post, {"https://m.ing.fr/secure/api-v1/login/sca/pin", '_', "application/json;charset=UTF-8", "{\"clickPositions\":"++ExpectedClickPositionsStr++"}"}, [], []],
+                        {ok, {{'fakeversion', 200, 'fakereason'}, [{"ingdf-auth-token", AuthToken}], "{\"name\":\"David\"}"}}
+                       }
+                      ],
+  NbrHttpcExpectations = length(HttpcExpectations),
+  meck:expect(httpc, request, HttpcExpectations),
+  {ok, {bank_auth, banks_fetch_bank_ing, AuthToken}} = banks_fetch_bank_ing:connect(?CLIENT_ID, {?CLIENT_PWD, ?CLIENT_BIRTHDATE}),
+  true = meck:validate(httpc),
+  NbrHttpcExpectations = meck:num_calls(httpc, request, '_'),
+
+  ok.
+
+should_connect(Config) ->
+  ct:comment("Load credential"),
+  case file:consult(filename:join([?config(data_dir, Config), "ing_real_credential.hrl"])) of
+    {error, enoent} ->
+      ct:comment("No credential, no real test"),
+      ok;
+    {ok, [{ClientId, {ClientPwd, ClientBirthDate}}]} ->
+      ct:comment("Connect to ing account"),
+      ok = httpc:set_options([{cookies,enabled}]),
+      {ok, {bank_auth, banks_fetch_bank_ing, _AuthToken}} = banks_fetch_bank_ing:connect(ClientId, {ClientPwd, ClientBirthDate}),
+      ok
+  end.
