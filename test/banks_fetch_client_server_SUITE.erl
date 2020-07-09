fc9a04f36975c2dec7f914321f73c237fcc91fcd
David JULIEN <david.julien@gmail.com>
2020-07-09 10:18:19 +0200

    Rename a test to highlight that we mock bank and storage

--- test/banks_fetch_client_server_SUITE.erl
+++ test/banks_fetch_client_server_SUITE.erl
@@ -1,7 +1,7 @@
 -module(banks_fetch_client_server_SUITE).
 -include_lib("common_test/include/ct.hrl").
 -export([all/0, init_per_testcase/2, end_per_testcase/2 ]).
--export([should_fetch_data/1, should_handle_cast_do_nothing/1]).
+-export([should_fetch_data_without_bank_storage/1, should_handle_cast_do_nothing/1]).
 
 -define(BANK_ID, {bank_id, <<"ing">>}).
 -define(CLIENT_ID, {client_id, <<"123456789">>}).
@@ -9,19 +9,19 @@
 
 all() -> [
           should_handle_cast_do_nothing,
-          should_fetch_data
+          should_fetch_data_without_bank_storage
          ].
 
 init_per_testcase(should_handle_cast_do_nothing, Config) ->
   Config;
-init_per_testcase(should_fetch_data, Config) ->
+init_per_testcase(should_fetch_data_without_bank_storage, Config) ->
   meck:new(banks_fetch_bank_ing),
   meck:new(banks_fetch_storage),
   Config.
 
 end_per_testcase(should_handle_cast_do_nothing, _Config) ->
   ok;
-end_per_testcase(should_fetch_data, _Config) ->
+end_per_testcase(should_fetch_data_without_bank_storage, _Config) ->
   meck:unload(banks_fetch_storage),
   meck:unload(banks_fetch_bank_ing).
 
@@ -29,15 +29,15 @@ should_handle_cast_do_nothing(_Config) ->
   {noreply, dummystate} = banks_fetch_client_server:handle_cast(dummycall, dummystate),
   ok.
 
-should_fetch_data(_Config) ->
+should_fetch_data_without_bank_storage(_Config) ->
   FakeAccounts = [account1, account2],
-  meck:expect(banks_fetch_bank_ing, connect, fun(MockClientId, MockCredential) -> 
+  meck:expect(banks_fetch_bank_ing, connect, fun(MockClientId, MockCredential) ->
                                                  ?CLIENT_ID = MockClientId,
                                                  ?CLIENT_CREDENTIAL = MockCredential,
                                                  {ok, fake_auth} end),
-  meck:expect(banks_fetch_bank_ing, fetch_accounts, fun(MockAuth) -> 
+  meck:expect(banks_fetch_bank_ing, fetch_accounts, fun(MockAuth) ->
                                                         fake_auth = MockAuth,
-                                                        {ok, FakeAccounts} 
+                                                        {ok, FakeAccounts}
                                                     end),
   BeforeFetchingDateTime = calendar:universal_time(),
   meck:expect(banks_fetch_storage, store_accounts, fun(MockBankId, MockClientId, MockFetchingAt, MockAccounts) ->
@@ -59,7 +59,7 @@ should_fetch_data(_Config) ->
   true = meck:validate(banks_fetch_bank_ing),
   true = meck:validate(banks_fetch_storage),
 
-  ct:comment("Verify that accounts contain fetched accounts"),
+  ct:comment("Verify that client server contain fetched accounts"),
   NewAccounts = banks_fetch_client_server:accounts(BanksPID),
   FakeAccounts = NewAccounts,
 
