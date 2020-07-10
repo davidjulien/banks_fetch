c73b39ee1476a0908e3c28be6d44a90236007ad4
David JULIEN <david.julien@gmail.com>
2020-07-10 13:35:02 +0200

    Add our first integration test.
    banks_fetch application is started and a new client is added (credential
    is read from an external configuration file). We verify that some accounts
    are inserted in storage.

---
+++ test/banks_fetch__integration_SUITE.erl
@@ -0,0 +1,71 @@
+-module(banks_fetch__integration_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-export([
+         all/0,
+         init_per_testcase/2,
+         end_per_testcase/2,
+
+         should_fetch_data_from_a_bank_and_store_them/1
+        ]).
+
+all() ->
+  [
+   should_fetch_data_from_a_bank_and_store_them
+  ].
+
+-define(DB_NAME, "banks_fetch_test").
+-define(DB_USER, "banks_fetch_test").
+-define(DB_PASSWORD, "").
+
+init_per_testcase(should_fetch_data_from_a_bank_and_store_them, Config) ->
+  ct:comment("Load credential"),
+  case file:consult(filename:join([?config(data_dir, Config), "real_credential.hrl"])) of
+    {error, enoent} ->
+      {skip, "Credential is required to test full integration"};
+    {ok, []} ->
+      {skip, "Credential is required to test full integration"};
+    {ok, [{BankId, ClientId, ClientCredential}]} ->
+      ct:comment("Start pgsql"),
+      ok = application:start(pgsql),
+
+      ct:comment("Reset test database"),
+      Connection = pgsql_connection:open("127.0.0.1", "postgres", ?DB_USER, ?DB_PASSWORD),
+
+      DropTestDbQuery = "DROP DATABASE IF EXISTS " ?DB_NAME,
+      {{drop, _}, []} = pgsql_connection:simple_query(DropTestDbQuery, Connection),
+      CreateTestDbQuery = "CREATE DATABASE " ?DB_NAME " WITH OWNER " ?DB_USER,
+      {{create, _}, []} = pgsql_connection:simple_query(CreateTestDbQuery, [], Connection),
+      pgsql_connection:close(Connection),
+
+      ok = application:set_env(banks_fetch, storage, {?DB_NAME, ?DB_USER, ?DB_PASSWORD}),
+
+      ct:comment("Start banks_fetch application"),
+      ok = application:start(banks_fetch),
+
+      % Mecking banks_fetch_storage allows to wait for store_accounts call
+      ok = meck:new(banks_fetch_storage, [passthrough]),
+
+      [{client_info, {BankId, ClientId, ClientCredential}}|Config]
+  end.
+
+end_per_testcase(should_fetch_data_from_a_bank_and_store_them, _Config) ->
+  meck:unload(banks_fetch_storage),
+  application:stop(banks_fetch),
+  application:stop(pgsql),
+  ok.
+
+
+should_fetch_data_from_a_bank_and_store_them(Config) ->
+  {client_info, {BankId, ClientId, ClientCredential}} = lists:keyfind(client_info, 1, Config),
+
+  ct:comment("Add new client"),
+  ok = banks_fetch_client_manager:add_client(BankId, ClientId, ClientCredential),
+
+  ct:comment("Wait for accounts storing"),
+  ok = meck:wait(banks_fetch_storage, store_accounts, '_', 20000),
+
+  ct:comment("Verify accounts data"),
+  {ok, Accounts} = banks_fetch_storage:get_accounts(BankId, ClientId),
+  [_|_] = Accounts,
+
+  ok.
