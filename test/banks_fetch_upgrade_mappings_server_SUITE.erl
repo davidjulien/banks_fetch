eada36f48fed6b2ebaa62c77c2f871f7efa09862
David JULIEN <david.julien@gmail.com>
2020-11-12 11:05:14 +0100

    Add a function to test current mappings file

--- test/banks_fetch_upgrade_mappings_server_SUITE.erl
+++ test/banks_fetch_upgrade_mappings_server_SUITE.erl
@@ -4,20 +4,22 @@
 -export([
          should_handle_cast_do_nothing/1,
          should_fetch_mappings_without_http_storage/1,
-         should_fetch_mappings_without_storage/1
+         should_fetch_mappings_without_storage/1,
+         should_test_local_mappings/1
         ]).
 
 -define(FAKE_DATA_JSON, binary_to_list(<<"{\"budgets\": [{\"id\": 1, \"name\": \"Courant\"}], \"categories\": [{\"id\": 1, \"name\": \"Alimentation\", \"up_category_id\": null},{\"id\": 2, \"name\": \"Supermarché\", \"up_category_id\": 1}], \"stores\": [{\"id\": 1, \"name\": \"LIDL\"}], \"mappings\": [{\"id\": 1, \"pattern\": \"(LIDL ST GERMAIN|LIDL CHAMBOURCY|2635 ST GERMAIN)($| )\", \"fix_date\": \"none\", \"period\": \"month\", \"budget_id\": 1, \"categories_id\": [1,2], \"store_id\": 1}]}"/utf8>>)).
 -define(FAKE_DATA_BUDGETS, [#{ id => 1, name => <<"Courant">>} ]).
 -define(FAKE_DATA_CATEGORIES, [#{ id => 1, name => <<"Alimentation">>, up_category_id => none }, #{ id => 2, name => <<"Supermarché"/utf8>>, up_category_id => 1 }]).
 -define(FAKE_DATA_STORES, [#{ id => 1, name => <<"LIDL">> }]).
--define(FAKE_DATA_MAPPINGS, [#{ id => 1, pattern => <<"(LIDL ST GERMAIN|LIDL CHAMBOURCY|2635 ST GERMAIN)($| )">>, fix_date => none, period => month, 
+-define(FAKE_DATA_MAPPINGS, [#{ id => 1, pattern => <<"(LIDL ST GERMAIN|LIDL CHAMBOURCY|2635 ST GERMAIN)($| )">>, fix_date => none, period => month,
                                 budget_id => 1, categories_id => [1,2], store_id => 1}]).
 
 all() -> [
           should_handle_cast_do_nothing,
           should_fetch_mappings_without_http_storage,
-          should_fetch_mappings_without_storage
+          should_fetch_mappings_without_storage,
+          should_test_local_mappings
          ].
 
 init_per_suite(Config) ->
@@ -34,6 +36,11 @@ init_per_testcase(should_fetch_mappings_without_http_storage, Config) ->
   meck:new(banks_fetch_storage),
   meck:new(timer, [unstick,passthrough]), % unstick because timer resides in sticky dir, passthrough because lager needs now_diff
   Config;
+init_per_testcase(should_test_local_mappings, Config) ->
+  meck:new(banks_fetch_http),
+  meck:new(banks_fetch_storage),
+  meck:new(timer, [unstick,passthrough]), % unstick because timer resides in sticky dir, passthrough because lager needs now_diff
+  Config;
 init_per_testcase(should_fetch_mappings_without_storage, Config) ->
   meck:new(banks_fetch_storage),
   meck:new(timer, [unstick,passthrough]), % unstick because timer resides in sticky dir, passthrough because lager needs now_diff
@@ -43,6 +50,11 @@ init_per_testcase(should_fetch_mappings_without_storage, Config) ->
 end_per_testcase(should_handle_cast_do_nothing, _Config) ->
   ok;
 end_per_testcase(should_fetch_mappings_without_http_storage, _Config) ->
+  meck:unload(timer),
+  meck:unload(banks_fetch_storage),
+  meck:unload(banks_fetch_http),
+  ok;
+end_per_testcase(should_test_local_mappings, _Config) ->
   meck:unload(timer),
   meck:unload(banks_fetch_storage),
   meck:unload(banks_fetch_http),
@@ -84,14 +96,12 @@ should_fetch_mappings_without_http_storage(_Config) ->
   ExpectedCategories = ?FAKE_DATA_CATEGORIES,
   ExpectedStores = ?FAKE_DATA_STORES,
   ExpectedMappings = ?FAKE_DATA_MAPPINGS,
-  meck:expect(banks_fetch_storage, upgrade_mappings, fun(MockBudgets, MockCategories, MockStores, MockMappings) -> 
-                                                         error_logger:info_msg("E=~p", [ExpectedCategories]),
-                                                         error_logger:info_msg("M=~p", [MockCategories]),
+  meck:expect(banks_fetch_storage, upgrade_mappings, fun(MockBudgets, MockCategories, MockStores, MockMappings) ->
                                                          ExpectedBudgets = MockBudgets,
                                                          ExpectedCategories = MockCategories,
                                                          ExpectedStores = MockStores,
                                                          ExpectedMappings = MockMappings,
-                                                         ok 
+                                                         ok
                                                      end),
 
   {ok, UpgradeServerPid} = banks_fetch_upgrade_mappings_server:start_link(),
@@ -119,7 +129,7 @@ should_fetch_mappings_without_storage(_Config) ->
                                      {ok, tref}
                                  end),
 
-  meck:expect(banks_fetch_storage, upgrade_mappings, fun(Budgets, Categories, Stores, Mappings) -> 
+  meck:expect(banks_fetch_storage, upgrade_mappings, fun(Budgets, Categories, Stores, Mappings) ->
                                                          check_data(Budgets, 5),
                                                          check_data(Categories, 167),
                                                          check_data(Stores, 44),
@@ -142,12 +152,60 @@ should_fetch_mappings_without_storage(_Config) ->
 
   ok.
 
+
+should_test_local_mappings(Config) ->
+  BeforeFetchingDateTime = calendar:universal_time(),
+
+  meck:expect(banks_fetch_http, setup_monitoring, fun(MockServername) ->
+                                                      "raw.githubusercontent.com" = MockServername,
+                                                      ok
+                                                  end),
+  meck:expect(banks_fetch_http, request, fun(MockMethod, MockURL, MockHTTPOptions, MockOptions) ->
+                                             get = MockMethod,
+                                             {_, []} = MockURL,
+                                             [] = MockHTTPOptions,
+                                             [] = MockOptions,
+                                             {ok, Data} = file:read_file(filename:join([?config(data_dir, Config), "../../../../../../resources/mappings.json"])),
+                                             {ok, {{"HTTP/1.1", 200, <<"OK">>}, [], binary_to_list(Data)}}
+                                         end),
+
+  meck:expect(timer, send_after, fun(MockTime, MockCall) ->
+                                     24 * 60 * 60 * 1000 = MockTime,
+                                     fetch_mappings = MockCall,
+                                     {ok, tref}
+                                 end),
+
+  meck:expect(banks_fetch_storage, upgrade_mappings, fun(MockBudgets, MockCategories, MockStores, MockMappings) ->
+                                                         check_data(MockBudgets, none),
+                                                         check_data(MockCategories, none),
+                                                         check_data(MockStores, none),
+                                                         check_data(MockMappings, none),
+                                                         ok
+                                                     end),
+
+  {ok, UpgradeServerPid} = banks_fetch_upgrade_mappings_server:start_link(),
+
+  ct:comment("Verify that last_fetch datetime has been updated"),
+  LastFetch = banks_fetch_upgrade_mappings_server:last_fetch(UpgradeServerPid),
+  true = LastFetch =/= none andalso LastFetch >= BeforeFetchingDateTime,
+
+  ct:comment("Verify that expected functions has been called"),
+  true = meck:validate(banks_fetch_http),
+  true = meck:validate(banks_fetch_storage),
+  true = meck:validate(timer),
+
+  ok.
+
+
+
 check_data(List, ExpectedCount) ->
   Nbr = length(List),
-  ExpectedCount = Nbr,
+  if ExpectedCount =/= none -> ExpectedCount = Nbr;
+     true -> ok
+  end,
   Ids = extract_ids(List),
   UniqueIds = lists:usort(Ids), % check id unicity
-  ExpectedCount = length(UniqueIds).
+  Nbr = length(UniqueIds).
 
 extract_ids(L) ->
   [ ID || #{ id := ID } <- L ].
